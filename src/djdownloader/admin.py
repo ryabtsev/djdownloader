b895122e698461b4e1d63336cb2b68f82e79056a
ryabtsev <e.ryabtsev@gmail.com>
2025-10-02 10:59:38 +0300

    improve admin

--- src/djdownloader/admin.py
+++ src/djdownloader/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('url', 'status')
